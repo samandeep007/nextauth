1b424d9ba4b403125fa22180e37054694770d459
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-06-23 15:28:57 -0600

    chore: Update npm dependencies
    This commit updates the npm dependencies in the `package.json` file. It includes updating `mongoose` to version 8.4.3, `next` to version 14.2.4, and `nodemailer` to version 6.9.14. Additionally, it updates the dev dependencies, including `@types/nodemailer` to version 6.4.15, `@types/react` and `@types/react-dom` to version 18, `eslint` to version 8, `eslint-config-next` to version 14.2.4, and `typescript` to version 5.

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,23 @@
+import mongoose, { connection } from 'mongoose';
+
+export async function connect (){
+    try {
+        
+        await mongoose.connect(process.env.MONGO_URI || '')
+        const connectionReference = mongoose.connection;
+
+        connectionReference.on('connected', ()=> {
+            console.log('MongoDB Connected');
+        })
+
+        connectionReference.on('error', (err)=>{
+            console.log("MongoDB connection error, please make sure db is up and running " + err);
+            process.exit()
+        })
+
+        
+    } catch (error) {
+        console.log('Something went wrong while connecting to the database');
+        console.log(error);
+    }
+}
---
+++ src/models/userModel.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+const userSchema = new mongoose.Schema({
+username: {
+    type: String,
+    required: [true, 'Please provide a username'],
+    unique: [true, 'Username already exists'],
+    trim: true,
+},
+email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    unique: [true, 'Email already exists'],
+},
+
+password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+},
+isVerified: {
+    type: Boolean,
+    default: false,
+},
+
+isAdmin: {
+    type:Boolean,
+    default: false
+},
+
+forgotPasswordToken: String,  // this will be sent to the user's email and will be used to reset the password
+forgotPasswordTokenExpiry: Date, 
+verifyToken: String,
+verifyTokenExpiry: Date
+
+},{timestamps: true});
+
+
+const User = mongoose.models.users || mongoose.model('users', userSchema); // if the model is already created, use it, else create a new one
+
+export default User;
