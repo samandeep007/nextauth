00773fc543fa27e01eb39b770b79799885313325
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-06-23 16:50:09 -0600

    chore: Update npm dependencies

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,46 @@
+import {connect} from '@/dbConfig/dbConfig';
+import User from '@/models/userModel';
+import {NextRequest, NextResponse} from 'next/server';
+import bcryptjs from 'bcryptjs';
+import {sendEmail} from '@/helpers/mailer'
+
+
+connect();
+
+export async function POST(request: NextRequest){
+    try {
+        const reqBody = await request.json();
+        const {username, email, password} = reqBody;
+
+        const user = await User.findOne({email})
+
+        if(user){
+            return NextResponse.json({error: "User Already Exists"}, {status: 400})
+        }
+
+        const salt = await bcryptjs.genSalt(10);
+        const encryptedPassword = await bcryptjs.hash(password, salt);
+        
+        const newUser = new User({
+            username: username,
+            email: email,
+            password: encryptedPassword
+        })
+
+        const savedUser = await newUser.save();
+        console.log(savedUser);
+
+        //send verification email
+        const sentMail = await sendEmail({email, emailType: "VERIFY", userId: username});
+
+        NextResponse.json({
+            message: "User Registered successfully",
+            status: 200,
+            user: savedUser.select("-password")
+            
+        })
+        
+    } catch (error: any) {
+        return NextResponse.json({error: error.message}, {status: 500})
+    }
+}
--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,25 +1,53 @@
+import User from '@/models/userModel';
+import bcryptjs from 'bcryptjs';
 import nodemailer from 'nodemailer'
 
-export const sendEmail = async ({email, emailType, userId}:{email: String, emailType: String, userId: Number}) => {
+export const sendEmail = async ({email, emailType, userId}:{email: string, emailType: string, userId: string}) => {
     try {
 
-        //TODO: configure mail for usage
+      const hashedToken = await bcryptjs.hash(userId.toString(), 10)
+      //You can use UUID package here as well
+
+        if(emailType === "VERIFY"){
+          await User.findByIdAndUpdate(userId, {
+            $set: {
+              verifyToken: hashedToken,
+              verifyTokenExpiry: Date.now() + 3600000
+            }
+          })
+        }
+
+        else if(emailType === "RESET"){
+          await User.findByIdAndUpdate(userId, {
+            $set: {
+              forgotPasswordToken: hashedToken,
+              forgotPasswordTokenExpiry: Date.now() + 3600000
+            }
+          })
+        }
+
 
         const transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false, // Use `true` for port 465, `false` for all other ports
-            auth: {
-              user: "maddison53@ethereal.email",
-              pass: "jn7jnAPss4f63QBp6D",
-            },
-          });
+          host: "sandbox.smtp.mailtrap.io",
+          port: 2525,
+          auth: {
+            user: process.env.MAILTRAP_USERNAME, // For testing purposes
+            pass: process.env.MAILTRAP_PASSWORD
+          }
+        });
 
           const mailOptions = {
             from: 'samandeep@singh.com',
             to: email, 
             subject: emailType === 'VERIFY' ? "Verify your email" : "Reset your password",
-            html: "<b>Hello world?</b>",
+            html: `<div style="background-color: rgb(245, 245, 245); width: 100%; margin: 0; padding: 0; height: auto">
+                  <div style="width: 95%; margin: 0 auto; background-color: black; padding: 30px; color: white; display: flex; align-items: center; justify-content: center; margin-top: 10px"><img src="https://imgs.search.brave.com/OV0evwyZ2XnkKMhawkWFQHwt3eLQHTqGOrAXpr7FRBo/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dC5icmFuZGZldGNo/LmlvL2lkMmFsdWUt/cngvaWRkaFcwWW1l/US5wbmc_dXBkYXRl/ZD0xNzE0NTU2MjMx/OTc4" alt="companyLogo" height="40rem" width="200rem"></div>
+                   <div style="padding: 20px; padding-bottom: 200px">
+                    Hi ${userId}! <br/>
+                   <p>Please click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY"? "verify your account" : "reset your password"} or copy and paste the link below in your browser.</p><br>
+                   ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+                   </div>
+                   <div style="width: 95%; margin: 0 auto; background-color: black; padding: 30px; color: white; text-align: center; font-family:Arial, Helvetica, sans-serif">Copyright 2024</div></div>`,
             };
 
           const mailResponse = await transporter.sendMail(mailOptions)
